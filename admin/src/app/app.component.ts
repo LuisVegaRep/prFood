0effa98cc4165ba2aceb1b62dac125223c80b8c0
LVega <vega.luisangel.cb37@gmail.com>
2022-10-21 03:35:42 -0700

    Added paths and directional login

--- admin/src/app/app.component.ts
+++ admin/src/app/app.component.ts
@@ -3,8 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'admin';
+  user = '';
+  pass = '';
+
+  form = true;
+  level = 0;
+
+  check() {
+    if (this.user == 'a' && this.pass == 'a') {
+      this.level = 1;
+      this.form = false;
+    }
+    if (this.user == 's' && this.pass == 's') {
+      this.level = 2;
+      this.form = false;
+    }
+  }
+
+  onKey(event: any, value: number) {
+    if (value == 1) this.user = event.target.value;
+    if (value == 2) this.pass = event.target.value;
+  }
 }
--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { WorkerComponent } from './components/worker/worker.component';
+import { MenuComponent } from './components/admin/menu/menu.component';
+import { WorkersComponent } from './components/admin/workers/workers.component';
+import { InventoryComponent } from './components/admin/inventory/inventory.component';
+import { SideComponent } from './components/admin/side/side.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    WorkerComponent,
+    MenuComponent,
+    WorkersComponent,
+    InventoryComponent,
+    SideComponent
   ],
   imports: [
     BrowserModule
